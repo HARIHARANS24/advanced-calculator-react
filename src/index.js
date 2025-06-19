5e37f76567a76ff299e1eebbf0bee237a508694f
HARIHARAN S <hariharan24hs@gmail.com>
2025-06-20 00:07:20 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';
-import CalculatorApp from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<CalculatorApp />);
